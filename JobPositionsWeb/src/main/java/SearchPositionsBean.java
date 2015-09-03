51efa58ac33639d54c1149e826709d7d05915a15
FilipaPedrosa <filipapedrosa@gmail.com>
2015-09-03 11:07:50 +0100

    alterações

--- JobPositionsWeb/src/main/java/SearchPositionsBean.java
+++ JobPositionsWeb/src/main/java/SearchPositionsBean.java
@@ -15,7 +15,7 @@ public class SearchPositionsBean implements Serializable {
 
 	public SearchPositionsBean() {
 		positions = new ArrayList<>();
-		for (int i = 0; i < 20; i++) {
+		for (int i = 0; i < 30; i++) {
 			positions.add(i);
 		}
 
